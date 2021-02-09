e9ec4dd542d41c2e5aa39921690c0918c26acbed
ron93 <ronmkare1@gmail.com>
2021-02-09 22:58:35 +0300

    grid_search hyperparam tuning -- data:reading and spliting train(X)and target(y) --classifier: randomForest --param-grid: parameters to conduct grid search

--- HyperParameter_Tuning/grid_search.py
+++ HyperParameter_Tuning/grid_search.py
@@ -6,3 +6,17 @@ from sklearn import metrics
 from sklearn import model_selection
 
 
+ if __name__ == "__main__":
+    df = pd.read_csv("../data/mobile-price/train.csv")
+    #train
+    X = df.drop("price_range",axis=1).values
+    y = df.price_range.values
+
+    #n_jobs = -1 ->to use all available processing availabel
+    classifier = ensemble.RandomForestClassifier(n_jobs=-1)
+
+    param_grid = {
+        "n_estimators": [100,200,300,400],
+        "max_depth" :[1,3,5,7],
+        "criterion" : ["gini","entropy"],
+    }
