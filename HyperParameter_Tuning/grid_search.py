23c18fbf46459249c9bc5ca20efaea17da6f0cb7
ron93 <ronmkare1@gmail.com>
2021-02-09 23:18:31 +0300

    Model selection -- GridSearchCV --fit model and print best score and best estimator

--- HyperParameter_Tuning/grid_search.py
+++ HyperParameter_Tuning/grid_search.py
@@ -4,10 +4,14 @@ import numpy as numpy
 from sklearn import ensemble
 from sklearn import metrics 
 from sklearn import model_selection
+from pathlib import Path
 
 
- if __name__ == "__main__":
-    df = pd.read_csv("../data/mobile-price/train.csv")
+if __name__ == "__main__":
+    data_folder = Path("../data/mobile-price/")
+
+    f = data_folder / "train.csv"
+    df = pd.read_csv(f)
     #train
     X = df.drop("price_range",axis=1).values
     y = df.price_range.values
@@ -20,3 +24,19 @@ from sklearn import model_selection
         "max_depth" :[1,3,5,7],
         "criterion" : ["gini","entropy"],
     }
+
+
+    #cv(cross validation) -> 5 fold
+    model = model_selection.GridSearchCV(
+        estimator = classifier,
+        param_grid=param_grid,
+        scoring="accuracy",
+        verbose=10,
+        n_jobs=1,
+        cv=5,
+    )
+
+    model.fit(X,y)
+
+    print(model.best_score_)
+    print(model._estimator_.get_params())
