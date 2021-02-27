b892a27e48d450e2a8a685a222473f1e3ae8968d
ron93 <ronmkare1@gmail.com>
2021-02-27 15:10:00 +0300

    main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/user/", response_model=User):
+def create_user(user: User):
+    return user
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
