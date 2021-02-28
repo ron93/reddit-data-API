f754ce6e10a000da741e6ee4db85dc75e39e312e
ron93 <ronmkare1@gmail.com>
2021-02-28 14:47:49 +0300

    models and schemas

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,23 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os, sys
+from dotenv import load_dotenv
+
+# ---------------- added code here -------------------------#
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+sys.path.append(BASE_DIR)
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# ---------------- added code here -------------------------#
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +31,14 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+# ---------------- added code here -------------------------#
+import models
+#------------------------------------------------------------#
+
+# ---------------- changed code here -------------------------#
+# here target_metadata was equal to None
+
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import uvicorn
 from fastapi import FastAPI
+#--------------- added code ------------------------#
+import os
+from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi_sqlalchemy import db
+from models import User as ModelUser
+from schema import User as SchemaUser
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+#---------------------------------------------------#
 
 app = FastAPI()
-
-@app.post("/user/", response_model=User):
-def create_user(user: User):
-    return user
+#--------------- added code ------------------------#
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+#---------------------------------------------------#
+#--------------- modified code ---------------------#
+@app.post("/user/", response_model=SchemaUser)
+def create_user(user: SchemaUser):
+    db_user = ModelUser(
+        first_name=user.first_name, last_name=user.last_name, age=user.age
+    )
+    db.session.add(db_user)
+    db.session.commit()
+    return db_user
+#---------------------------------------------------#
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
