9cc463d01e8d6c72b11cd66fa74c74635e2cb8be
ron93 <ronmkare1@gmail.com>
2021-03-04 16:36:19 +0300

    reddit data views

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('main', __name__)
+
+@main.route('reddits', methods=['GET'])
+def reddits():
+    reddits = []
+    return jsonify({'reddits':reddits})
+
+@main.route('reddit/<id>/' methods=['GET'])
+def reddit(id):
+    reddit = []
+    return jsonify({'reddit':reddit})
