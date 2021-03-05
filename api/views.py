34f2b94bb0d355eef89ff6da34badebba4a0c1bb
ron93 <ronmkare1@gmail.com>
2021-03-05 10:21:25 +0300

    reddit/<id> rouute

--- api/views.py
+++ api/views.py
@@ -5,14 +5,16 @@ main = Blueprint('main', __name__)
 
 @main.route('/threads', methods=['GET'])
 def threads():
-    thread_list = Thread.query.all()
+    thread_list = Thread.query.filter(Thread.thread != "")
     reddits = []
 
     for th in thread_list:
         reddits.append({'thread':th.thread, 'sentiment':th.sentiment,'time':th.time })
     return jsonify({'reddits':reddits})
-'''
+
 @main.route('/reddit/<id>/' ,methods=['GET'])
 def reddit(id):
+    th = Thread.query.filter(Thread.id == id)
     reddit = []
-    return jsonify({'reddit':reddit})'''
+    reddit.append({'thread':th.thread, 'sentiment':th.sentiment,'time':th.time })
+    return jsonify({'reddit':reddit})
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Reddit Data API
 Flask API for reddit data and sentiments analysed from the comments
- ##Endpoints
- `/threads`
+ ## Endpoints <br>
+ `/threads` <br>
 
 `/thread/<id>/`
 
